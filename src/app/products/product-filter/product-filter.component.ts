0f992ff4fad35ef6fe21c8dba03735cf7f15c427
kirvi456 <201404243@ingenieria.usac.edu.gt>
2018-09-21 17:14:50 -0600

    Agregado componente Pregunta

---
+++ src/app/products/product-filter/product-filter.component.ts
@@ -0,0 +1,17 @@
+import { CategoryService } from './../../services/category.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'product-filter',
+  templateUrl: './product-filter.component.html',
+  styleUrls: ['./product-filter.component.css']
+})
+export class ProductFilterComponent {
+  categories$;
+  @Input('category') category;
+
+  constructor(private categoryService:CategoryService) {
+    this.categories$ = categoryService.getAll();
+  }
+
+}
