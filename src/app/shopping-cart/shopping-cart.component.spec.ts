9559ef53aa12bddf781d0ad2156d4f8e00f6269b
Jenny Montenegro <jennysharai14@gmail.com>
2018-09-21 16:31:08 -0600

    v3.0: Agregando pruebas unitarias - Shopping cart

--- src/app/shopping-cart/shopping-cart.component.spec.ts
+++ src/app/shopping-cart/shopping-cart.component.spec.ts
@@ -14,6 +14,46 @@ describe("ShoppingCartComponent", () => {
     component = new ShoppingCartComponent(service);
   });
 
+  it("ShoppingCartComponent deberia crearse", () => {
+    expect(service).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
 
+  it("Deberia obtener la propiedad cart$ del objeto retornado del servicio", () => {
+    let spy = spyOn(service, "getCart").and.returnValue(
+      Observable.from([new ShoppingCart(null)])
+    );
+
+    component.ngOnInit();
+
+    expect(spy).toHaveBeenCalled();
+    expect(component.cart$).not.toBeNull();
+  });
+
+  it("Deberia llamar al servicio de shopping cart para liberar todos los items del carro de compras", () => {
+    let spy = spyOn(service, "clearCart").and.callFake(() => {
+      return Observable.empty;
+    });
+
+    component.clearCart();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  
+  it("Deberia retornar 2 como cantidad total de items en el carro para ser comprado", () => {
+    expect(component).toBe(2);
+  });
+
+  it("Deberia llamar al servicio de shopping cart para liberar todos los items del carro de compras", () => {
+    let spy = spyOn(service, "clearCart").and.callFake(() => {
+      return Observable.empty;
+    });
+  });
+
+  it("Deberia crear una instancia con 0 productos en el carro", () => {
+    expect(component).toBeTruthy();
+    expect(component.clearCart);
+  });
 
 });
